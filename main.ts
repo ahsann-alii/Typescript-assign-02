12d2ccd49067fcf6d20fe6d16a73986e83f64cfc
ahsann-alii <ahsanzafar165@gmail.com>
2024-03-08 02:10:18 -0800

    first commit

---
+++ main.ts
@@ -0,0 +1,5 @@
+let name: string = "Ahsan";//to save the name
+
+console.log(name.toLowerCase());//for lower case
+console.log(name.toUpperCase());//for upper case
+console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());//for title case
